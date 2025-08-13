df47b25141f7b90f6b23d60080b103147c9870f6
LeTanThuan <letanthuan@dtu.edu.vn>
2025-08-14 00:54:49 +0700

    test_github

---
+++ src/Lec2_var/Ex1.java
@@ -0,0 +1,10 @@
+package Lec2_var;
+public class Ex1 {
+    public static void main(String[] args){
+        int a= 5;
+        int b= a++;      
+        System.out.println("a=" +a);
+        System.out.println("b=" +b);
+        System.out.println("asf");
+    }
+}
