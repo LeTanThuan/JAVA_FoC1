bc3756bad46425857b1d34009a1fca45cb228673
LeTanThuan <letanthuan@dtu.edu.vn>
2025-08-21 16:47:57 +0700

    Xong

---
+++ src/Lec5Loop_Exercise/Ex10_Draw.java
@@ -0,0 +1,30 @@
+
+package Lec5Loop_Exercise;
+
+import java.util.Scanner;
+
+public class Ex10_Draw {
+public static void DrawTriangle(int h){
+    int a=h;
+    int b=1;
+    for (int i=1 ; i<= h ; i++){
+        for (int j = 1; j <= a; j++){
+            System.out.print(" ");
+        }
+        a--;
+        for (int k= 1; k<=b; k++){
+            System.out.print("*");
+        }
+        b=b+2;
+        System.out.println("");
+    }
+    System.out.print("");
+}
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter High Triangle: ");
+        int high = scanner.nextInt();
+        DrawTriangle(high);
+    }
+}
---
+++ src/Lec5Loop_Exercise/Ex11_RollDoubles.java
@@ -0,0 +1,20 @@
+package Lec5Loop_Exercise;
+
+import java.util.*;
+
+public class Ex11_RollDoubles {
+    public static void main(String[] args){
+        Random random = new Random();
+        int ranDomA ;
+        int ranDomB;
+        int count=0;
+        do{
+            ranDomA = random.nextInt(6) +1;
+            ranDomB = random.nextInt(6) +1; 
+            count++;
+        }while (ranDomA != ranDomB);
+        System.out.println("Repeat :" + count);
+        System.out.println(ranDomA);
+        System.out.println(ranDomB);
+    }
+}
