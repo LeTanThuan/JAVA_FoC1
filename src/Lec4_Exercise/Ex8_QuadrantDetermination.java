b798fe8ac1a866c9684150f297ffed8c9ae52c67
LeTanThuan <letanthuan@dtu.edu.vn>
2025-08-22 16:02:06 +0700

    Xong

--- src/Lec4_Exercise/Ex8_QuadrantDetermination.java
+++ src/Lec4_Exercise/Ex8_QuadrantDetermination.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Lec4_Exercise;
 
-/**
- *
- * @author PC MSI
- */
+import java.util.Scanner;
+
 public class Ex8_QuadrantDetermination {
-    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter X: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter Y: ");
+        double y = scanner.nextDouble();
+        if (x>0 && y > 0){
+            System.out.println(" Quadrant 1: ");
+        }else if(x < 0 && y > 0){
+            System.out.println("Quadrant 2:");
+        }else if(x < 0 && y < 0){
+            System.out.println("Quadrant 3:");
+        }else if (x > 0 && y < 0){
+            System.out.println("Quadrant 4:");
+        }else if (x==0 && y==0){
+            System.out.println("The point is on an axis.");
+        }
+        
+    }
 }
