5e724b4822a92bd1bcb476c99db51f39ded3b6a0
LeTanThuan <letanthuan@dtu.edu.vn>
2025-08-28 16:29:29 +0700

    Xong

---
+++ src/Lec7_String/Slide21_Ex2.java
@@ -0,0 +1,11 @@
+package Lec7_String;
+
+public class Slide21_Ex2 {
+
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "HELLO";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+}
---
+++ src/Lec7_String/Slide24_Ex4.java
@@ -0,0 +1,15 @@
+
+
+package Lec7_String;
+
+
+public class Slide24_Ex4 {
+    public static void main(String[] args){
+        String s1="Hello";
+        String s2=new String("Hello");
+        String s3= "HelloWorld";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s3.compareTo(s1));
+    }
+}
